fb352dbe93203fbc81533e82e381c941aa53ea6c
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-05-25 13:36:41 +0400

    21.3 fix and start with Material Design

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -8,7 +8,9 @@ const PageNav = () => (
   <nav>
     <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName="active"> Home Page </Button>
     <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/login`} activeClassName="active"> Login </Button>
-
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/tables`} activeClassName="active"> Tables </Button>
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/waiter`} activeClassName="active"> Waiter </Button>
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName="active"> Kitchen </Button>
     
   </nav>
 );
